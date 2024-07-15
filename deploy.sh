9cea186509f90516d6f6cd9f332bf611bea617e0
azmusgb <36898133+azmusgb@users.noreply.github.com>
2024-07-15 04:47:42 +0000

    Deploy from Replit

--- deploy.sh
+++ deploy.sh
@@ -25,15 +25,6 @@ if ! command -v netlify &> /dev/null; then
     npm install -g netlify-cli
 fi
 
-# Create virtual environment
-python3.8 -m venv venv
-
-# Activate virtual environment
-source venv/bin/activate
-
-# Install dependencies
-pip install -r requirements.txt
-
 # Deploy to Netlify
 export NETLIFY_AUTH_TOKEN=${NETLIFY_AUTH_TOKEN}
-netlify deploy --prod --dir=static --site=a3b5bb87-099a-46d2-aed3-f50ed90fbc96
+netlify deploy --prod --dir=. --site=a3b5bb87-099a-46d2-aed3-f50ed90fbc96
